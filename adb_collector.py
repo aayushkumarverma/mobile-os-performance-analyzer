dce5f29d9e4048381998a3728842557cf3a6ecbd
aayushkumarverma <aayushkumarverma807@gmail.com>
2026-09-12 09:53:16 +0530

    Create adb_collector.py

---
+++ adb_collector.py
@@ -0,0 +1,10 @@
+"""
+This module will communicate with the Android device using ADB.
+
+Later it will collect:
+- Running processes
+- CPU usage
+- Memory information
+- Thread counts
+- Battery information
+"""
